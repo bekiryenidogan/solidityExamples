20ee7ed97d3140cbce7e94f80b00c942d3ae56ec
"Bekir Yenidogan" <"bekiryenidogan16@gmail.com">
2022-11-01 14:37:39 +0300

    Reverse String

---
+++ ReverseString.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.1 <0.9.0;
+
+contract ReverseString {
+    function reverseString ( string calldata _str) external pure  returns (string memory){
+        bytes memory str = bytes(_str);
+        string memory tmp = new string (str.length);
+        bytes memory _reversed = bytes(tmp);
+
+        for (uint i = 0; i<str.length; i++) {
+            _reversed[str.length -i - 1]= str[i];
+        }
+        return string (_reversed);
+    }
+}
